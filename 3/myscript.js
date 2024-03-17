067b4bff3f7971fd2721b525d763118d75eb7b6f
vishalupadhyay7389 <120776857+vishalupadhyay7389@users.noreply.github.com>
2024-03-17 19:49:25 +0530

    first commit

---
+++ 3/myscript.js
@@ -0,0 +1,10 @@
+// The sign of single line comment is // double slash
+//document.write("This is vishal kumar upadhyay");
+
+
+
+// The sign of multiple comment in javascript is /*
+
+/*document.write("<b>This is vishal kumar upadhyay javascripts page </b><br><br>");
+
+document.write("<h2><i>this is letter from honrable prime minister of india nadrendra damodar das modi jii</i> </h2>");*/
---
+++ javaScript_variable/myscripts.js
@@ -0,0 +1,4 @@
+var vishal = "This is vishal kumar upadhyay"
+var vishal2 = "This is vishal kumar upadhyay page is here"
+document.write(vishal);
+document.write(vishal2)
